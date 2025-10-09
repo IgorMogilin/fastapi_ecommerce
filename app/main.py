5be1141a7db6d33aec456ea985d17ca8175711a7
Igor Mogilin <endless8space@gmail.com>
2025-10-09 11:51:52 +0300

    Add logging

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+from uuid import uuid4
+from fastapi import FastAPI, Request
+
+from fastapi.responses import JSONResponse
+from loguru import logger
 
 from app.routers import categories, products, reviews, users
 
@@ -6,6 +10,33 @@ app = FastAPI(
     title="FastAPI Интернет-магазин",
     version="1.0",
 )
+
+logger.add(
+    "info.log",
+    format="Log: [{extra[log_id]}:{time} - {level} - {message}]",
+    level="INFO",
+    enqueue=True,
+)
+
+
+@app.middleware("http")
+async def log_middleware(request: Request, call_next):
+    log_id = str(uuid4())
+    with logger.contextualize(log_id=log_id):
+        try:
+            response = await call_next(request)
+            if response.status_code in [401, 402, 403, 404]:
+                logger.warning(f"Request to {request.url.path} failed")
+            else:
+                logger.info('Successfully accessed ' + request.url.path)
+        except Exception as ex:
+            logger.error(f"Request to {request.url.path} failed: {ex}")
+            response = JSONResponse(
+                content={"success": False},
+                status_code=500,
+            )
+        return response
+
 
 app.include_router(categories.router)
 app.include_router(products.router)
